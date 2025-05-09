7c549be4c214ea63e2b0f4f018c238bfb716be2b
Marc Lange <m@lange-data.org>
2025-05-09 15:18:42 +0200

    new ips

---
+++ db_lib/pg_conn.py
@@ -0,0 +1,16 @@
+def get_connection_creds():
+    from pathlib import Path
+    p = Path(__file__).absolute().parent.parent
+    p = p / "dockers" / "databases" / ".env"
+    with open(p,'r') as fi:
+        s = fi.readlines()
+    s = list(map(lambda z: tuple(z.split('=')),s))
+    s = list(map(lambda z: (z[0],z[1].strip('\n')),s))
+    s = dict(s)
+    return s
+
+def get_postgres_connection():
+    creds = get_connection_creds()
+    from sqlalchemy import create_engine
+    engine = create_engine(f'postgresql+psycopg2://{creds['POSTGRES_USER']}:{creds['POSTGRES_PASSWORD']}@localhost/{creds['POSTGRES_DB']}')
+    return engine
