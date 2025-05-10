9e0c5fff14d63ab71bae7b49389a5739c426da0b
Marc Lange <m@lange-data.org>
2025-05-10 23:01:30 +0200

    deactivate submissions in full

--- reddit_lib/reddit_full_sequence.sh
+++ reddit_lib/reddit_full_sequence.sh
@@ -1,5 +1,5 @@
 echo "running full"
-python3 reddit-00-load_submissions.py
+# python3 reddit-00-load_submissions.py
 python3 reddit-01-load_comments.py
 # python3 reddit-02-compress_tables.py
 # python3 db_lib/postgres_backup.py
