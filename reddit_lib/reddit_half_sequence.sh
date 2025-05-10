8329e94bd50623ae5e0ee84d47a8cff8fe3578e1
Marc Lange <m@lange-data.org>
2025-05-10 22:29:04 +0200

    deactivate comments in half

--- reddit_lib/reddit_half_sequence.sh
+++ reddit_lib/reddit_half_sequence.sh
@@ -1,4 +1,4 @@
 python3 reddit-00-load_submissions.py
-python3 reddit-01-load_comments.py
+# python3 reddit-01-load_comments.py
 # python3 reddit-02-compress_tables.py
 # python3 db_lib/postgres_backup.py
