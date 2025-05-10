4d3c451c806af081afd07a2744eb4a91e763282e
Marc Lange <m@lange-data.org>
2025-05-10 20:28:42 +0200

    removed backups and compression from sequence, too RAM heavy

--- reddit_lib/db_lib/postgres_backup.py
+++ reddit_lib/db_lib/postgres_backup.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import pg_conn
 eng = pg_conn.get_postgres_connection()
+pg_con = eng.connect().execution_options(stream_results=True)
 
 from time import time as _t
 time = lambda : int(_t())
@@ -11,7 +12,7 @@ with open(p,'r') as fi:
     backup_db_path = ''.join(fi.readlines())
 
 import sqlite3
-con = sqlite3.connect(backup_db_path + f'reddit_{time()}.db')
+con = sqlite3.connect(backup_db_path + '/' + f'reddit_{time()}.db')
 
 all_tables = list(pd.read_sql("select table_name from information_schema.tables where table_schema = 'public'",eng).table_name.unique())
 all_tables = sorted(all_tables)
@@ -19,7 +20,7 @@ all_tables = sorted(all_tables)
 for table in all_tables:
     print('starting', table)
     start_t = time()
-    df = pd.read_sql(f"select * from {table}",eng)
+    df = pd.read_sql(f"select * from {table}",eng)    
     if 'id' in df.columns:
         df = df.drop(columns='id')
     df.to_sql(table,con,if_exists='replace',index=False)
